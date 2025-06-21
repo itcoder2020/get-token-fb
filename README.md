1f92eefacf9a6a10dd6936d08de8b9a6dcba5149
ภานุพงศ์ นิยะกิจ <jkjknm05x@gmail.com>
2025-06-21 11:17:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 🔐 Facebook Token Generator
+
+## 📦 การติดตั้งและใช้งาน
+
+โปรเจกต์นี้ต้องการ **Node.js เวอร์ชัน 17 ขึ้นไป** และ **npm** เพื่อจัดการ dependencies
+
+### ✅ ขั้นตอนการติดตั้ง
+
+1. ติดตั้ง Node.js (แนะนำเวอร์ชัน 17 ขึ้นไป)  
+   ดาวน์โหลดได้ที่: [https://nodejs.org/](https://nodejs.org/)
+
+2. ติดตั้ง dependencies  
+   เปิด Terminal หรือ Command Prompt แล้วรันคำสั่ง:
+
+   ```bash
+   npm install
+
+### ✅ หากรับโทเค้นไม่ได้ไห้ยืนยันสิทธ์ผ่านลิ้ง
+https://www.facebook.com/dialog/oauth?client_id=124024574287414&redirect_uri=https://www.instagram.com/accounts/signup/&&scope=email,publish_actions,manage_pages,pages_show_list,publish_pages,ads_management,business_management,public_profile&response_type=token
---
+++ app.js
@@ -0,0 +1,45 @@
+const axios = require("axios")
+async function getToken(cookie) {
+    try {
+        if (!cookie && cookie === "") throw new Error("input cookie")
+        const headers = {
+            'Origin': 'https://www.instagram.com',
+            'Accept-Language': 'id,en;q=0.9',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            'Referer': 'https://www.instagram.com/',
+            'Host': 'www.facebook.com',
+            'Sec-Fetch-Mode': 'cors',
+            'Accept': '*/*',
+            'Cookie':cookie,
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Dest': 'empty',
+            'Accept-Encoding': 'gzip, deflate',
+        }
+        const params = new URLSearchParams( {
+            'client_id': '124024574287414',
+            'wants_cookie_data': 'true',
+            'origin': '1',
+            'input_token': '',
+            'sdk': 'joey',
+            'redirect_uri': 'https://www.instagram.com/ijolovemom/',
+        })
+        const response = await axios.get("https://www.facebook.com/x/oauth/status",{params,headers})
+        token = JSON.stringify(response.headers)
+        const match = token.match(/(EAAB\w+)/)
+        if(!match) throw new Error("cannot get token")
+        return match[0]
+    } catch (error) {
+        if (error.response) {
+
+            console.log(error.response.error)
+
+        } else {
+            console.log(error.message)
+        }
+
+    }
+
+
+}
+getToken("")
