bbc254c81219ce13a5310187db11f202283dca1e
ภานุพงศ์ นิยะกิจ <jkjknm05x@gmail.com>
2025-06-21 11:22:33 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 async function getToken(cookie) {
     try {
-        if (!cookie && cookie === "") throw new Error("input cookie")
+        if (!cookie || cookie === "") throw new Error("input cookie")
         const headers = {
             'Origin': 'https://www.instagram.com',
             'Accept-Language': 'id,en;q=0.9',
@@ -25,14 +25,14 @@ async function getToken(cookie) {
             'redirect_uri': 'https://www.instagram.com/ijolovemom/',
         })
         const response = await axios.get("https://www.facebook.com/x/oauth/status",{params,headers})
-        token = JSON.stringify(response.headers)
+        const token = JSON.stringify(response.headers)
         const match = token.match(/(EAAB\w+)/)
         if(!match) throw new Error("cannot get token")
         return match[0]
     } catch (error) {
         if (error.response) {
 
-            console.log(error.response.error)
+            console.log(error.response.data)
 
         } else {
             console.log(error.message)
